08e860782d88bd234f6b34c68f8f5dfca29410b6
shan ali khoso <38635542+shanAliKhos@users.noreply.github.com>
2020-11-04 02:32:50 +0500

    asd

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,37 +39,37 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request)
     {
-        // return array_merge(parent::share($request), [
-        //     "SiteOptions" => [
-        //         "Title" => Setting::get('site_title'),
-        //         "Logo" => Setting::get('site_logo'),
-        //         "Favicon" => Setting::get('site_favicon'),
-        //         "Email" => Setting::get('default_email_address'),
-        //         "Currency" => [  
-        //             "Code" => Setting::get('currency_code'),
-        //             "Symbol" => Setting::get('currency_symbol'),
-        //         ],
-        //         "Social" => [
-        //             "Facebook"=> Setting::get('social_facebook'),
-        //             "Twitter"=> Setting::get('social_twitter'),
-        //             "Instagram"=> Setting::get('social_instagram'),
-        //             "LinkedIn"=> Setting::get('social_linkedin'),
-        //         ], 
-        //         "FooterCopyRightText" => Setting::get('footer_copyright_text'),
-        //     ],
-        //     'Categories'=> Category::where(['is_active'=>true,'menu'=>true])->get(),
-        //     'Cart' => function () {
-        //         return [
-        //             'Items' => Session::get('CartItems'),
-        //         ];
-        //     },
-        //     'flash' => function () {
-        //         return [
-        //             'success' => Session::get('success'),
-        //             'error' => Session::get('error'), 
-        //             'info' => Session::get('info'),
-        //         ];
-        //     },
-        // ]);
+        return array_merge(parent::share($request), [
+            // "SiteOptions" => [
+            //     "Title" => Setting::get('site_title'),
+            //     "Logo" => Setting::get('site_logo'),
+            //     "Favicon" => Setting::get('site_favicon'),
+            //     "Email" => Setting::get('default_email_address'),
+            //     "Currency" => [  
+            //         "Code" => Setting::get('currency_code'),
+            //         "Symbol" => Setting::get('currency_symbol'),
+            //     ],
+            //     "Social" => [
+            //         "Facebook"=> Setting::get('social_facebook'),
+            //         "Twitter"=> Setting::get('social_twitter'),
+            //         "Instagram"=> Setting::get('social_instagram'),
+            //         "LinkedIn"=> Setting::get('social_linkedin'),
+            //     ], 
+            //     "FooterCopyRightText" => Setting::get('footer_copyright_text'),
+            // ],
+            // 'Categories'=> Category::where(['is_active'=>true,'menu'=>true])->get(),
+            // 'Cart' => function () {
+            //     return [
+            //         'Items' => Session::get('CartItems'),
+            //     ];
+            // },
+            // 'flash' => function () {
+            //     return [
+            //         'success' => Session::get('success'),
+            //         'error' => Session::get('error'), 
+            //         'info' => Session::get('info'),
+            //     ];
+            // },
+        ]);
     }
 }
