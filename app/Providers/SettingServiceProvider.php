f25c1260a2ffd0ed0c924bfab88da361a7995ad5
shan ali khoso <38635542+shanAliKhos@users.noreply.github.com>
2020-11-04 03:43:33 +0500

    fgdgdfgdgfdfgd

--- app/Providers/SettingServiceProvider.php
+++ app/Providers/SettingServiceProvider.php
@@ -22,11 +22,11 @@ class SettingServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind('settings', function ($app) {
-            return new Setting();
-        });
-        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-        $loader->alias('Setting', Setting::class);
+        // $this->app->bind('settings', function ($app) {
+        //     return new Setting();
+        // });
+        // $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+        // $loader->alias('Setting', Setting::class);
  
     }
 
@@ -38,12 +38,12 @@ class SettingServiceProvider extends ServiceProvider
     public function boot()
     {
         // only use the Settings package if the Settings table is present in the database
-        if (!\App::runningInConsole() && count(Schema::getColumnListing('settings'))) {
-            $settings = Setting::all();
-            foreach ($settings as $key => $setting)
-            {
-                Config::set('settings.'.$setting->key, $setting->value);
-            }
-        }
+        // if (!\App::runningInConsole() && count(Schema::getColumnListing('settings'))) {
+        //     $settings = Setting::all();
+        //     foreach ($settings as $key => $setting)
+        //     {
+        //         Config::set('settings.'.$setting->key, $setting->value);
+        //     }
+        // }
     }
 }
