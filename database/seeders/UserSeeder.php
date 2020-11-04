8cff1159109a86be657432b125d57c2e9298cbde
shan ali khoso <38635542+shanAliKhos@users.noreply.github.com>
2020-11-04 22:55:57 +0500

    sadasd

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,6 +18,7 @@ class UserSeeder extends Seeder
                 'name' => Str::random(10),
                 'email' => Str::random(10).'@gmail.com',
                 'email_verified_at' => now(),
+                'is_admin' => ($i==0)?1:0,
                 'password' => Hash::make('password'),
                 'remember_token' => Str::random(10),
             ]);
