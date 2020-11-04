20532501c61fdc9018d7013bff1bee557ed2c8be
shan ali khoso <38635542+shanAliKhos@users.noreply.github.com>
2020-11-04 23:16:11 +0500

    asdasdasd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,14 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     { 
-        $this->call(SettingsTableSeeder::class);
-        $this->call(UserSeeder::class);  
         $this->call(CategoriesTableSeeder::class);
+        $this->call(SettingsTableSeeder::class);
+        $this->call(AttributesTableSeeder::class);
+        $this->call(AttributeValuesTableSeeder::class); 
+        $this->call(UserSeeder::class);  
         // \App\Models\Brand::factory(20)->create();  
         // \App\Models\Product::factory(100)->create(); 
         // \App\Models\ProductCategory::factory(100)->create(); 
-        $this->call(AttributesTableSeeder::class);
-        $this->call(AttributeValuesTableSeeder::class); 
         // $this->call(ProductAttributSeeder::class); 
     }
 }
