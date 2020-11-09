b14783ba5d677db9ae894a9e6de7fe8c24ea00ca
shan ali khoso <38635542+shanAliKhos@users.noreply.github.com>
2020-11-10 00:05:54 +0500

    sdad

---
+++ database/seeders/ProductCategorySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\ProductCategory;
+
+
+class ProductCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        for ($i=1; $i <= 50; $i++) {
+
+            DB::table('product_attributes')->insert([
+                'category_id'=>rand(2,11),
+                'product_id'=>rand(1,50),
+            ]);
+
+        }  
+
+    }
+}
