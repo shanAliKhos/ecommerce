65f04e1246c6fbdc861c6aa472918165beebe82f
shan ali khoso <38635542+shanAliKhos@users.noreply.github.com>
2020-11-10 19:52:09 +0500

    shadow update in nav

--- resources/js/Pages/Ecomerce/shared/components/NavLink.vue
+++ resources/js/Pages/Ecomerce/shared/components/NavLink.vue
@@ -13,15 +13,9 @@
         computed: {
             IsActive() {
                 return this.active
-                            ? 'transition duration-500 ease-in-out active  shadow   mr-4 border border-green-500 btn active-fonts'
-                            : 'transition duration-500 ease-in-out hover:shadow p-1 mr-4 mr-4  border-green-500 btn' 
+                            ? 'transition duration-500 ease-in-out active  shadow-md   mr-4 border border-green-500 btn'
+                            : 'transition duration-500 ease-in-out hover:shadow-md p-1 mr-4 mr-4  border-green-500 btn' 
             }
         }
     }
-</script>
-<style scoped>
-.active-fonts{
-    font-size:15px !important;
-}
-    
-</style>
+</script> 
