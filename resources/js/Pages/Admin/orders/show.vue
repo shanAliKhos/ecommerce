aa96bd0156e1f63e727c6b572d00136dc57be3fa
shan ali khoso <38635542+shanAliKhos@users.noreply.github.com>
2020-11-12 04:53:45 +0500

    dad

--- resources/js/Pages/Admin/orders/show.vue
+++ resources/js/Pages/Admin/orders/show.vue
@@ -14,36 +14,39 @@
                 <h3 class="tile-title"><i class="fa fa-globe"></i> {{ Order.OrderNumber }}</h3>
                 <div class="tile-body">
                     <section class="invoice"> 
-                        <div class="row invoice-info">
-                            <div class="col-4">
-                                <span>
-                                    Placed By
-                                    <address><strong>{{ Order.user.name }}</strong><br>Email: {{ Order.user.email }}</address>
-                                </span>
-                                <span>
-                                     <address><strong>Created_at  - </strong>{{ Order.created_at }}</address>
-                                     <address><strong>Updated_at  - </strong>{{ Order.updated_at }}</address>
-                                    
-                                </span> 
+                        <div class="invoice-info">
+                            <div class="row">
+                                <div class="col-12 col-md-4 sm-4">
+                                    <span>
+                                        Placed By
+                                        <address><strong>{{ Order.user.name }}</strong><br>Email: {{ Order.user.email }}</address>
+                                    </span>
+                                    <span>
+                                        <address><strong>Created_at  - </strong>{{ Order.created_at }}</address>
+                                        <address><strong>Updated_at  - </strong>{{ Order.updated_at }}</address>
+                                        
+                                    </span> 
 
-                            </div>
-                            <div class="col-4">Ship To
-                                <address>
-                                    <strong>{{ Order.CardHolderName }}</strong>
-                                    <br>{{ Order.Address }}<br>{{ Order.City }}, {{ Order.Country }} {{ Order.PostalCode }}<br>{{ Order.PhoneNumber }}<br>
-                                </address>
-                            </div>
-                            <div class="col-4">
-                            
-                                <b>Order ID:</b> {{ Order.OrderNumber }}<br>
-                                <b>Order Status:</b>  
-                                <i class="fa" :class="StatusIcon" aria-hidden="true"> <span> {{Order.Status}}</span> </i> 
-                                <br>
-                                <b>Amount:</b> ${{ Order.GrandTotal  }}<br>
-                                <!-- <b>Amount:</b> {{ config('settings.currency_symbol') }}{{ round(Order.grand_total, 2) }}<br> -->
-                                <b>Payment Method:</b> {{ Order.PaymentMethod }}<br>
-                                <b>Payment Status:</b> {{ Order.PaymentStatus == 1 ? 'Completed' : 'Not Completed' }}<br>
-                                <b>Payment Track:</b> {{ Order.PaymentToken }}<br>
+                                </div>
+                                <div class="col-12 col-md-4 sm-4">Ship To
+                                    <address>
+                                        <strong>{{ Order.CardHolderName }}</strong>
+                                        <br>{{ Order.Address }}<br>{{ Order.City }}, {{ Order.Country }} {{ Order.PostalCode }}<br>{{ Order.PhoneNumber }}<br>
+                                    </address>
+                                </div>
+                                <div class="col-12 col-md-4 sm-4">
+                                    <span>
+                                    <b>Order ID:</b> {{ Order.OrderNumber }}<br>
+                                    <b>Order Status:</b>  
+                                    <i class="fa" :class="StatusIcon" aria-hidden="true"> <span> {{Order.Status}}</span> </i> 
+                                    <br>
+                                    <b>Amount:</b> ${{ Order.GrandTotal  }}<br>
+                                    <!-- <b>Amount:</b> {{ config('settings.currency_symbol') }}{{ round(Order.grand_total, 2) }}<br> -->
+                                    <b>Payment Method:</b> {{ Order.PaymentMethod }}<br>
+                                    <b>Payment Status:</b> {{ Order.PaymentStatus == 1 ? 'Completed' : 'Not Completed' }}<br>
+                                    <b>Payment Track:</b> {{ Order.PaymentToken }}<br>
+                                    </span>
+                                </div>
                             </div>
                         </div>
                         <div class="row">
@@ -73,31 +76,32 @@
                     </section>
                 </div>
                 <div class="tile-footer">
-                    <div class="row d-print-none mt-2">
-                        <div class="col-12 text-right">
-                            <!-- <inertia-link v-if="(Order.Status == 'pending')"  -->
-                            <inertia-link  
+                    <div class="row"> 
+                        <div class="btn-group" role="group">
+                            
+                            <inertia-link  class="btn btn-warning"
                                 v-if="Order.Status !== 'pending'"
-                                as="button" type="button" class="btn btn-warning"
+                                as="button" type="button" 
                                 :data="{ Status: 'pending' }"  
                                 method="put" preserve-scroll 
                                 :href="$route('admin.order.update', this.Order.id)"  
                                 >
                                 <i class="fa fa-fw fa-lg fa-circle-o-notch" ></i><span>Pending</span>
                             </inertia-link>
-                            <inertia-link  
+                            
+                            <inertia-link  class="btn btn-info"
                                 v-if="Order.Status !== 'processing'"
-                                as="button" type="button" class="btn btn-info"
+                                as="button" type="button" 
                                 :data="{ Status: 'processing' }"  
                                 method="put" preserve-scroll 
                                 :href="$route('admin.order.update', this.Order.id)"  
                                 >
                                 <i class="fa fa-fw fa-lg fa-circle-o-notch" ></i><span>Process</span>
                             </inertia-link>
-                            <!-- <inertia-link v-if="(Order.Status == 'processing')"  -->
-                            <inertia-link  
+                            
+                            <inertia-link  class="btn btn-success"
                                 v-if="Order.Status !== 'completed'"
-                                as="button" type="button" class="btn btn-success"
+                                as="button" type="button" 
                                 method="put" preserve-scroll 
                                 :data="{ Status: 'completed' }"  
                                 :href="$route('admin.order.update', this.Order.id)" 
@@ -105,16 +109,16 @@
                                 <i class="fa fa-fw fa-lg fa-check-circle"></i><span>Completed</span>
                             </inertia-link>
 
-                            <!-- <inertia-link v-if="(Order.Status == 'processing')"  -->
-                            <inertia-link  
+                            <inertia-link   class="btn btn-danger"
                                 v-if="Order.Status !== 'decline'"
-                                as="button" type="button" class="btn btn-danger"
+                                as="button" type="button"
                                 method="delete" preserve-scroll  
                                 :href="$route('admin.order.destroy', this.Order.id)" 
                                 >
                                 <i class="fa fa-fw fa-lg fa-remove"></i> <span>Cancell</span>
                             </inertia-link>
-                        </div>
+
+                        </div> 
                     </div>
                 </div>                
             </div>
