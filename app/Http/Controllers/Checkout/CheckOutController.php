68a023e2dcb73e12514c742e3375ed36fc59d0ec
shan ali khoso <38635542+shanAliKhos@users.noreply.github.com>
2020-11-11 16:10:14 +0500

    email validate in checkout

--- app/Http/Controllers/Checkout/CheckOutController.php
+++ app/Http/Controllers/Checkout/CheckOutController.php
@@ -36,7 +36,7 @@ class CheckOutController extends Controller
         }
         
         $this->validate($request,[ 
-            "email" => "required|email",
+            "email" => "required|email:rfc,dns",
             "name_on_card" => "required",
             "address" => "required",
             "city" => "required",
