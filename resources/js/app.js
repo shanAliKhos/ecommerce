b76dfb1e6213b98e34b6072c0b85a9bed8fbc514
shan ali khoso <38635542+shanAliKhos@users.noreply.github.com>
2020-11-11 21:30:25 +0500

    fixes

--- resources/js/app.js
+++ resources/js/app.js
@@ -27,7 +27,7 @@ InertiaProgress.init({
   delay: 250,
 
   // The color of the progress bar.
-  color: '#29d',
+  color: '#7fad39',
 
   // Whether to include the default NProgress styles.
   includeCSS: true,
