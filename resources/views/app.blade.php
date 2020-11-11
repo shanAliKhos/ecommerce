8525ccc5526bdbb488bd156da1cb54f80cf0d304
shan ali khoso <38635542+shanAliKhos@users.noreply.github.com>
2020-11-11 22:21:52 +0500

    layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,6 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;900&display=swap" rel="stylesheet">
         {{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"> --}}
-
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <link rel="stylesheet" href="/css/frontend/bootstrap.min.css" type="text/css">
@@ -18,31 +17,29 @@
         <link rel="stylesheet" href="/css/frontend/jquery-ui.min.css" type="text/css">
         <link rel="stylesheet" href="/css/frontend/owl.carousel.min.css" type="text/css">
         <link rel="stylesheet" href="/css/frontend/slicknav.min.css" type="text/css">
-        <link rel="stylesheet" href="/css/frontend/style.css" type="text/css">         
-        
+        <link rel="stylesheet" href="/css/frontend/style.css" type="text/css">                
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment.min.js"></script>
         <script src="https://js.stripe.com/v3/"></script>
 
         @routes
-
  
     </head>  
     
-    <body class="app sidebar-mini rtl  font-sans antialiased  ">
+    <body class="app sidebar-mini rtl font-sans antialiased ">
        
         @inertia
 
-    <script src="/js/frontend/jquery-3.3.1.min.js"></script>
-    <script src="/js/frontend/bootstrap.min.js"></script> 
-    {{-- <script src="{{ asset('backend/js/plugins/pace.min.js') }}"></script> --}}
-    <script src="/js/frontend/jquery.nice-select.min.js"></script>
-    <script src="/js/frontend/jquery-ui.min.js"></script>
-    <script src="/js/frontend/jquery.slicknav.js"></script>
-    <script src="/js/frontend/mixitup.min.js"></script>
-    <script src="/js/frontend/owl.carousel.min.js"></script>
-    <script src="/js/frontend/main.js"></script>          
-   
+        <script src="/js/frontend/jquery-3.3.1.min.js"></script>
+        <script src="/js/frontend/bootstrap.min.js"></script> 
+        {{-- <script src="{{ asset('backend/js/plugins/pace.min.js') }}"></script> --}}
+        <script src="/js/frontend/jquery.nice-select.min.js"></script>
+        <script src="/js/frontend/jquery-ui.min.js"></script>
+        <script src="/js/frontend/jquery.slicknav.js"></script>
+        <script src="/js/frontend/mixitup.min.js"></script>
+        <script src="/js/frontend/owl.carousel.min.js"></script>
+        <script src="/js/frontend/main.js"></script>        
+
     </body>
 </html>
