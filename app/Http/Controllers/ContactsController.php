4eb06cb7a462577ae20780d59bd5e5829be013b5
shan ali khoso <38635542+shanAliKhos@users.noreply.github.com>
2020-12-08 16:34:06 +0500

    k

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -27,20 +27,20 @@ class ContactsController extends Controller
 
         // get a collection of items where sender_id is the user who sent us a message
         // and messages_count is the number of unread messages we have from him
-        $unreadIds = Message::select(DB::raw('`from` as sender_id, count(`from`) as messages_count'))
-            ->where('to', auth()->id())
-            ->where('read', false)
-            ->groupBy('from')
-            ->get();
+        // $unreadIds = Message::select(DB::raw('`from` as sender_id, count(`from`) as messages_count'))
+        //     ->where('to', auth()->id())
+        //     ->where('read', false)
+        //     ->groupBy('from')
+        //     ->get();
 
         // add an unread key to each contact with the count of unread messages
-        $contacts = $contacts->map(function($contact) use ($unreadIds) {
-            $contactUnread = $unreadIds->where('sender_id', $contact->id)->first();
+        // $contacts = $contacts->map(function($contact) use ($unreadIds) {
+        //     $contactUnread = $unreadIds->where('sender_id', $contact->id)->first();
 
-            $contact->unread = $contactUnread ? $contactUnread->messages_count : 0;
+        //     $contact->unread = $contactUnread ? $contactUnread->messages_count : 0;
 
-            return $contact;
-        });
+        //     return $contact;
+        // });
 
 
         return response()->json($contacts);
