86a7c03a71e8c789b675ecd48c099e6c880e5213
shan ali khoso <38635542+shanAliKhos@users.noreply.github.com>
2020-12-19 06:36:26 +0500

    hero

--- resources/js/Pages/Ecomerce/shared/Slider/HeroSlider.vue
+++ resources/js/Pages/Ecomerce/shared/Slider/HeroSlider.vue
@@ -11,16 +11,11 @@
                 <div class="sm:bg-center bg-no-repeat bg-object lg:bg-right bg-gray-50" 
                     :style="'background-image:url(\'' + HeroSlide.photo_url + '\')'">
                     <div class="py-36 px-5 sm:px-10 md:px-12 xl:px-24 text-center sm:text-left sm:w-5/6 lg:w-3/4 xl:w-2/3 ">
-                        <p class="font-butlermedium text-secondary text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
-                            {{HeroSlide.title}}
-                        </p>
+                        <p class="font-butlermedium text-secondary text-3xl sm:text-4xl md:text-5xl lg:text-6xl">{{HeroSlide.title}}</p>
                         <inertia-link :href="HeroSlide.button_url" class="btn btn-primary btn-lg mt-8">{{HeroSlide.button_title}}</inertia-link>
                     </div>
                 </div>            
-            </vue-glide-slide>  
- 
-
-
+            </vue-glide-slide>   
         </vue-glide>
  
 </template>
