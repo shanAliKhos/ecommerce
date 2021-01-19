e052d74e2bc0285f49e3e05ba0a5d040044a881a
shan ali khoso <38635542+shanAliKhos@users.noreply.github.com>
2021-01-19 21:44:43 +0500

    navigation

--- app/Http/Controllers/Ecommerce/CartController.php
+++ app/Http/Controllers/Ecommerce/CartController.php
@@ -27,8 +27,7 @@ class CartController extends Controller
     }    
  
     public function store(Request $request)
-    {   
-        dd($request->all());
+    {    
         $this->validate($request,[
             'id'=>'required|numeric',
             'name'=>'required',
